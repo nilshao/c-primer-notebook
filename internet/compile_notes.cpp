af7458ded800bd311c0277e424f9fbecd5e52553
nilshao <nielhaochina@gmail.com>
2020-12-05 23:30:12 +0100

    internet chapter1

---
+++ internet/compile_notes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+std::string NOTELIST;
+std::string OUTPUTLIST;
+
+void CompileTogether(){
+    
+}
+
+int main(){
+    CompileTogether();
+    return 0;
+}
