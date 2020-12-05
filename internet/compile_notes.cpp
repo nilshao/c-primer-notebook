11561e13ed71c8049eda3d6c9c18104997223643
nilshao <nielhaochina@gmail.com>
2020-12-05 23:51:30 +0100

    internet chapter1

--- internet/compile_notes.cpp
+++ internet/compile_notes.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 std::string NOTELIST;
 std::string OUTPUTLIST;
 
 void CompileTogether(){
-    
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    char data;
+     
+    if (!baifin || !fout)
+        return 0;
+         
+    while(!fin.eof())
+    {
+        fin>>data;
+        fout<<data;
+    }
+ 
+    return 0;
 }
 
 int main(){
