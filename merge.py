08bb5daeac27128b3c2c2ac28d4863b3dae79521
nilshao <nielhaochina@gmail.com>
2020-12-28 00:35:12 +0100

    upload

--- merge.py
+++ merge.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
 #merge internet
-internet_file = ["internet/seperate_notes/lesson1.md",
+ori_file = ["internet/seperate_notes/lesson1.md",
                 "internet/seperate_notes/lesson2.md",
                 "internet/seperate_notes/lesson3.md",
                 "internet/seperate_notes/lesson4.md",
                 "internet/seperate_notes/lesson5.md",
                 "internet/seperate_notes/lesson6.md"]
-merge_internet_dir = "internet/internet_merged.md"
-merged_note = open(merge_internet_dir,'w')
-for str in internet_file:
+target_file = "internet/internet_merged.md"
+merged_note = open(target_file,'w')
+for str in ori_file:
     seperate_note = open(str,'r').readlines()
     merged_note.writelines(seperate_note)
 
 #merge operation system
-os_file = ["operation_system/notes/Chapter01.md",
+ori_file = ["operation_system/notes/Chapter01.md",
             "operation_system/notes/Chapter02_1.md",
             "operation_system/notes/Chapter02_2.md",
             "operation_system/notes/Chapter02_3.md",
@@ -24,8 +24,21 @@ os_file = ["operation_system/notes/Chapter01.md",
             "operation_system/notes/Chapter04_1.md",
             "operation_system/notes/Chapter04_2.md",
             "operation_system/notes/Chapter05.md"]
-merge_os_dir= "operation_system/operation_system_merged.md"
-merged_note = open(merge_os_dir,'w')
-for str in os_file:
+target_file = "operation_system/operation_system_merged.md"
+merged_note = open(target_file,'w')
+for str in ori_file:
+    seperate_note = open(str,'r').readlines()
+    merged_note.writelines(seperate_note)
+
+#merge C++Primer
+ori_file = ["C++Primer/chapter01.md",
+                    "C++Primer/chapter02.md",
+                    "C++Primer/chapter03.md",
+                    "C++Primer/chapter06.md",
+                    "C++Primer/chapter07.md",
+                    "C++Primer/chapter10.md"]
+target_file = "C++Primer/notes.md"
+merged_note = open(target_file,'w')
+for str in ori_file:
     seperate_note = open(str,'r').readlines()
     merged_note.writelines(seperate_note)
