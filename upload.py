be11ea12a83c4e6d8839f31e7c44cfa095896775
nilshao <nielhaochina@gmail.com>
2020-12-30 00:28:10 +0100

    Update, !

--- upload.py
+++ upload.py
@@ -1,11 +1,11 @@
 #!/bin/sh
 #!/usr/bin/python
 
-update_info = 'update'
+update_info = "test"
 
 chmod +x merge.py 
 ./merge.py
 
 git add .
-git commit -m "Update \"$update_info\""
+git commit -m "Update,"$update_info" !"
 git push
